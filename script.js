13d2844a8790e115f179d55eeb3e6f5651aee692
Kate <nekl47511@gmail.com>
2020-12-02 23:17:50 +0200

    Counting movies

--- script.js
+++ script.js
@@ -9,11 +9,35 @@ const personalMovieDB = {
     genres: [],
     privat: false,
 };
-const a = +prompt('Сколько фильмов вы уже посмотрели?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Сколько фильмов вы уже посмотрели?', ''),
-    d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+let i = 0;
+do {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+
+
+    if (a != '' && b != '' && a.length < 50 && b.length < 50 && a != null && b != null) {
+
+        personalMovieDB.movies[a] = b;
+        console.log('Done!');
+        i++;
+    } else {
+        console.log('error!');
+        i--;
+    }
+} while (i < 2)
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
+
+
+
 console.log(personalMovieDB);
